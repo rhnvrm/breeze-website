8b725ce5235603614a19d1ef9c1a86eef1f85d11
Harshit Budhraja <harshitbudhraja@users.noreply.github.com>
2016-07-03 16:27:47 +0530

    Removed the countdown timer and deleted all the codes. (#27)
    * Removed html code for timer
    
    * Removed the calling function for timer
    
    * Deleted the countdown timer js code

--- js/grayscale.js
+++ js/grayscale.js
@@ -186,16 +186,6 @@ function init() {
     });
 }
 
-
-$(function(){
-    $("#future_date").countdowntimer({
-        dateAndTime : "2016/10/22 00:00:00",
-        size : "lg",
-        regexpMatchFormat: "([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})",
-        regexpReplaceWith: "$1<sup>days</sup> $2<sup>hours</sup> $3<sup>minutes</sup> $4<sup>seconds</sup>"
-    });
-});
-
 var div = $('.intro');
 var interval;
 var counter = 0;
